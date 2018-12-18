6ac847d78a4b703d802ab290d134d1424c362b97
jemikanegara <jemikanegara@gmail.com>
2018-12-18 17:24:57 +0800

    finish navigation with @output on header component and onNavigate on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'ng-project';
+export class AppComponent implements OnInit {
+  title = "ng-project";
+  loadedFeature: string;
+  constructor() {
+    this.loadedFeature = "recipe";
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
+  ngOnInit() {}
 }
