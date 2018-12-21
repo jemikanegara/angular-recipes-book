273ab5f3f0050a36df9f119ca570b7d3f0ab176f
jemikanegara <jemikanegara@gmail.com>
2018-12-21 11:44:10 +0800

    add ingredient list to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,20 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter, Output } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
-      "A Test Recipe",
-      "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "Tasty Schnitzel",
+      "A Super Tasty Schnitzel",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
     ),
     new Recipe(
       "Ayam Bakar",
       "a food from indonesia",
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Ayam_bakar_Aie_Badarun.JPG/800px-Ayam_bakar_Aie_Badarun.JPG"
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Ayam_bakar_Aie_Badarun.JPG/800px-Ayam_bakar_Aie_Badarun.JPG",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
     )
   ];
 
